857312522aaa896acd485f3d80d65d14f15b5bc4
Gean Brandao <karlos867gean@gmail.com>
2020-06-09 14:00:03 -0300

    finish

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransationDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,52 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumlator: Balance, transation: Transaction) => {
+        const accumlatorReturn = accumlator; // por causa do erro de resign
+        switch (transation.type) {
+          case 'income':
+            accumlatorReturn.income += transation.value;
+            break;
+          case 'outcome':
+            accumlatorReturn.outcome += transation.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumlatorReturn;
+      },
+      {
+        // formato do objeto retornado
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransationDTO): Transaction {
     // TODO
+
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,27 @@
-import { Router } from 'express';
+import { Router, json } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+/**
+ * O service é o reponsavel pela regra de negocios
+ */
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+    const createTranssationService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +29,28 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    if (type === 'outcome') {
+      const { total } = transactionsRepository.getBalance();
+      if (total < value) {
+        return response
+          .status(400)
+          .json({ error: 'Youre balance is insuficient' });
+      }
+    }
+
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
